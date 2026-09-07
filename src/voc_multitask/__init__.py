0862cf5f12b97fd5c64d86c8a0a2dfb11b2fd903
Tanjim Hossain <57143091+Tanjim-hossain@users.noreply.github.com>
2026-09-07 23:29:05 +0200

    feat: add reusable VOC package

---
+++ src/voc_multitask/__init__.py
@@ -0,0 +1,7 @@
+"""Reusable components for the PASCAL VOC2012 multi-task vision project."""
+
+VOC_CLASSES = (
+    "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat",
+    "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person",
+    "pottedplant", "sheep", "sofa", "train", "tvmonitor",
+)
